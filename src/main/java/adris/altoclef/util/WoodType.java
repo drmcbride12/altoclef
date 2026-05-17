8b6a14f195dfdad3cc76547523b916855cbf2340
drmcbride12 <drmcbride12@users.noreply.github.com>
2026-05-17 18:05:38 -0400

    Add modern wood families to resource targets

--- src/main/java/adris/altoclef/util/WoodType.java
+++ src/main/java/adris/altoclef/util/WoodType.java
@@ -3,10 +3,13 @@ package adris.altoclef.util;
 public enum WoodType {
     ACACIA,
     BIRCH,
+    CHERRY,
     CRIMSON,
     DARK_OAK,
+    MANGROVE,
     OAK,
     JUNGLE,
+    PALE_OAK,
     SPRUCE,
     WARPED
 }
